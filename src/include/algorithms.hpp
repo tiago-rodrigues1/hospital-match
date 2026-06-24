1036674f1351ac4d64687d842df148e8d5ece09c
Jamilly Emilly da Silva Campelo <jamilly.s.campelo@gmail.com>
2026-06-24 18:12:49 -0300

    feat/implements algorithms 3, 4 and 5

--- src/include/algorithms.hpp
+++ src/include/algorithms.hpp
@@ -7,10 +7,21 @@
 #include "./edge.hpp"
 
 const int INF = 99999999;
-const int MULTIPLIER = 10000; // Pra dar certo, tem que arredondar os custos pra no máximo 4 casas decimais
+const int MULTIPLIER = 10000;
 
 std::vector<std::vector<int>> buildCostMatrix(const std::vector<Patient>& patients,
                                               const std::vector<HospitalBed>& beds,
                                               const std::vector<Edge>& viableEdges);
+
+int searchAugmentingPath(int currentPatient,
+                         const std::vector<std::vector<int>>& costMatrix,
+                         std::vector<int>& u,
+                         std::vector<int>& v,
+                         std::vector<int>& p,
+                         std::vector<int>& path);
+
+void updateMatching(std::vector<int>& p, std::vector<int>& path, int j0);
+
+std::vector<int> hungarianAlgorithm(const std::vector<std::vector<int>>& costMatrix, int n);
 
 #endif
