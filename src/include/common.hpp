8b15064926161202b1e7ce2156890a4c2cc7afbd
tiago-rodrigues1 <tiagords127@gmail.com>
2026-06-25 14:50:02 -0300

    feat: create enum for parsers types and struct for user running options

--- src/include/common.hpp
+++ src/include/common.hpp
@@ -2,6 +2,8 @@
 #define COMMON_HPP
 
 #include <cmath>
+#include <cstdint>
+#include <filesystem>
 
 struct Location {
   double latitude;
@@ -10,6 +12,17 @@ struct Location {
   double getDistanceTo(Location loc) {
     return std::sqrt(std::pow(loc.latitude - latitude, 2) + std::pow(loc.longitude - longitude, 2));
   }
+};
+
+enum parserType_e : std::uint8_t {
+  OR_LIB_DENSE = 0,
+  OR_LIB_SPARSE,
+  NON_OR_LIB_RP 
+};
+
+struct RunningOpt {
+  parserType_e parser;
+  std::string inputPath;
 };
 
 #endif
